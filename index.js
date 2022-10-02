3ee93099ba02a4653cfe16a4db9e8223f27d121c
Paulo Silas <paulo3356@gmail.com>
2022-10-01 23:33:32 -0300

    iniciando testes

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express')
+const fs = require('fs')
+
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const data = "Paulo sbbandeira"
+const fileTxt = "arquivo.txt"
+
+var teste
+
+fs.readFile('arquivo.txt','utf-8', (err, data) => {
+    if (err) throw err;
+    teste = data
+    console.log(teste);
+  });
+/*
+fs.writeFile(fileTxt, data, (err) => {
+    if(err) throw err;
+    console.log('arquivo criado')
+})
+*/
+
+app.get('/', (req, res) => res.send(teste))
 
 app.listen(process.env.PORT || 3000)
